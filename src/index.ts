91113a547b0e790e088d284d171f180f41e1389c
avraham <2721avi@gmail.com>
2024-10-01 15:02:30 +0300

    add static folder

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ const app: Express = express();
 const nodeServer = createServer(app);
 const ioServer = new Server(nodeServer);
 
+app.use("/front", express.static("frontend"));
 app.use("/auth");
 app.use("/games");
 
