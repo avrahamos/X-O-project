8a4086a1152e7d9ae7ec5402b83a61ec6a447ac4
avraham <2721avi@gmail.com>
2024-10-01 15:29:41 +0300

    add auth service rgister and login

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ const app: Express = express();
 const nodeServer = createServer(app);
 const ioServer = new Server(nodeServer);
 
+app.use(express.json());
 app.use("/front", express.static("frontend"));
 app.use("/auth");
 app.use("/games");
---
+++ src/services/authService.ts
@@ -0,0 +1,53 @@
+import bcrypt from "bcrypt";
+import User from "../types/models/userModel";
+import { generateJWT } from "../utils/ganreteToken";
+import { getFileData, saveFileData } from "../config/getData";
+
+export default class AuthService {
+  public async register(
+    userName: string,
+    password: string
+  ): Promise<User | null> {
+    try {
+      const users: User[] = (await getFileData<User>("users")) || [];
+
+      const userExists = users.find((u) => u.userName === userName);
+      if (userExists) {
+        return null;
+      }
+
+      const newUser = new User(userName);
+      await newUser.hashPassword(password);
+      users.push(newUser);
+
+      await saveFileData<User>("users", users);
+      return newUser;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+
+  public async login(
+    userName: string,
+    password: string
+  ): Promise<string | null> {
+    try {
+      const users: User[] = (await getFileData<User>("users")) || [];
+      const user = users.find((u) => u.userName === userName);
+      if (!user) {
+        return null;
+      }
+
+      const isMatch = await user.comparePassword(password);
+      if (isMatch) {
+        return generateJWT(user);
+      }
+
+      return null;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+}
